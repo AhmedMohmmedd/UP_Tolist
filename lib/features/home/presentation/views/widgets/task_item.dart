32dd88da7f0961f163f226ecff8041ec046baab1
Ahmed Mohmmedd <ahmedmkersha22@gmail.com>
2024-10-29 10:12:46 +0300

    merge home

--- lib/features/home/presentation/views/widgets/task_item.dart
+++ lib/features/home/presentation/views/widgets/task_item.dart
@@ -1,7 +1,4 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:up_tolist/core/constansts/asset_images.dart';
 import 'package:up_tolist/core/theming/app_color.dart';
